bf8361d9d93aecbd436f5f2b71a7f53a3dd96533
jsump <libbyn79@gmail.com>
2024-04-03 12:02:45 +0000

    Return least recently used cache

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+"""
+Module: 3-lru_cache.py
+
+This module contains a class that inherits from BaseCaching
+"""
+
+
+from base_caching import BaseCaching
+
+
+class LRUCache(BaseCaching):
+    """
+    This class inherits from BaseCaching
+    """
+    def __init__(self):
+        """
+        This method is for initializtion
+        """
+        super().__init__()
+        self.keys_in_order = []
+
+    def put(self, key, item):
+        """
+        This method checks if the number of items in cached data
+        is higher and returns the data linked to key
+        """
+        if key is not None and item is not None:
+            if len(self.cache_data) >= self.MAX_ITEMS:
+                discarded_key = self.keys_in_order.pop(0)
+                del self.cache_data[discarded_key]
+                print(f"DISCARD: {discarded_key}")
+            self.cache_data[key] = item
+            if key in self.keys_in_order:
+                self.keys_in_order.remove(key)
+            self.keys_in_order.append(key)
+
+    def get(self, key):
+        """
+        This method returns value of data returned to key
+        """
+        if key is not None and key in self.cache_data:
+            self.keys_in_order.remove(key)
+            self.keys_in_order.append(key)
+            return self.cache_data[key]
+        return None
