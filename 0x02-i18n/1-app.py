e5e16c257c2983e3480813ac88a5f051dcf8dfb0
jsump <libbyn79@gmail.com>
2024-04-09 12:39:43 +0000

    Babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Module: 1-app.py
+
+This module contains a basic FLASK APP
+"""
+
+
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    """
+    This class contains language attributes
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.fromobject(Config)
+babel = Babel(app)
+
+
+if __name__ == "__main__":
+    app.run()
