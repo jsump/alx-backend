c65f354cd1eb7e2affb5d7eb51fb9224da7a2a4d
jsump <libbyn79@gmail.com>
2024-04-09 13:06:50 +0000

    Full code

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -30,7 +30,7 @@ def index():
     """
     This method returns the htmls template
     """
-    return render_template('0-index.html')
+    return render_template('1-index.html')
 
 
 if __name__ == "__main__":
--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -10,18 +10,36 @@ from flask import Flask, render_template
 from flask_babel import Babel
 
 app = Flask(__name__)
-babel = babel(app)
 
+
+class Config:
+    """
+    This class contains language attributes
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app.config.from_object(Config)
+babel = Babel(app)
 
 app.config['LANGUAGES'] = ["en", "fr"]
-
 
 @babel.localselector
 def get_locale():
     """
-    This method determines the best match with the supported languages
+    Thie method determines the best match to supported languages
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def index():
+    """
+    This method returns the htmls template
+    """
+    return render_template('1-index.html')
 
 
 if __name__ == "__main__":
