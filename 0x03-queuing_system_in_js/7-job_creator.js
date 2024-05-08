e5138031749b486c58959f2f54582a20e2e835ee
jsump <libbyn79@gmail.com>
2024-05-08 16:37:36 +0300

    Task 8 edit

--- 0x03-queuing_system_in_js/7-job_creator.js
+++ 0x03-queuing_system_in_js/7-job_creator.js
@@ -78,3 +78,50 @@ jobs.forEach((jobData) => {
     pushNotificationQueue.save();
 
 });
+
+// start queue
+queue.process('push_notification_code_2', (job,done) => {
+    setTimeout(() => {
+        job.progress(50, 100);
+
+        const random = Math.random();
+        if (random <0.5) {
+            done();
+        } else {
+            done(new Error('Error'));
+        }
+    }, Math.random() * 5000);
+});
+
+// Listen to job processes on queue
+queue.process('push_notification_code', (job, done) => {
+    const { phoneNumber, message } = job.data;
+    sendNotification(phoneNumber, message);
+    done();
+});
+
+// start queue
+queue.on('ready', () => {
+    console.log('Ready');
+});
+
+// Error
+queue.on('error', (err) => {
+    console.log('Error', err);
+});
+
+// Process termination
+process.once('SIGTERM', () => {
+    queue.shutdown(5000, (err) => {
+        console.log('Shutdown');
+        process.exit(0)
+    });
+});
+
+process.once('SIGINT', () => {
+    queue.shutdown(5000, (err) => {
+        console.log('Shutdown');
+        process.exit(0)
+    });
+});
+
