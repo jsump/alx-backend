5c4a03bd39d49d2b5b86a5fbfce6973f94f133f1
jsump <libbyn79@gmail.com>
2024-05-08 12:33:08 +0000

    REmove connecton

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -1,22 +1,8 @@
 /**
  * Create job creator
  */
-import redis from 'redis';
-
-const client = redis.createClient();
-
 const kue = require('kue');
 const queue = kue.createQueue();
-
-// When connection workds correctly
-client.on('connect', () => {
-    console.log('Redis client connected to the server');
-});
-
-// when Connection to Redis does not work
-client.on('error', (error) => {
-    console.log(`Redis client not connected to the server: ${error}`);
-});
 
 // Creat job object
 const jobObject = {
